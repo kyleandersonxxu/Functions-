e11990217a93e99d854d1553271132b2706e7220
Kyle Anderson <kyleandersonxxu@gmail.com>
2021-04-05 08:42:09 -0600

    initial commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# Get information from the user about conversion
+# TODO User input validation 
+# TODO Convert the string to a number 
+userInput = input ('what is the number: ')
+print ('Your number is:', userInput)
+
+userNumber = int(userInput)
+
+print (type(userNumber))
+# Preform the conversion
+# Print out the answer to the user  
